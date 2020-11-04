c40bd32ed217449c67649bd3ca4acb7f197f812d
深蓝 <mail@ri-co.cn>
2020-11-04 22:14:15 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <topbar></topbar>
+  </div>
+</template>
+
+<script>
+import header from '@/views/header.vue'
+export default {
+  components:{
+    header:topbar
+  }
+}
+</script>
