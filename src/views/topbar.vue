d2df0bab8f005b84797e84f4e70f735f7ef45e60
深蓝 <mail@ri-co.cn>
2020-11-04 22:31:48 +0800

    完成 登录功能

--- src/views/topbar.vue
+++ src/views/topbar.vue
@@ -1,9 +1,6 @@
 <template>
-  <v-card
-    color="grey lighten-4"
-  >
-    <v-toolbar  
-      src="https://api.ri-co.cn/whisper/getpics/390d8fd0-VaranasiCandles_EN-AU12230572751_1920x1080.jpg">
+  <v-card color="grey lighten-4">
+    <v-toolbar>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
       <v-toolbar-title>深蓝电商平台</v-toolbar-title>
@@ -13,16 +10,11 @@
       <v-btn>首页</v-btn>
       <v-btn>分类</v-btn>
       <v-btn>购物车</v-btn>
-      <v-btn @click="loginWin=true">登录</v-btn>
+      <v-btn @click="loginWin = true">登录</v-btn>
 
       <v-menu bottom left>
         <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            dark
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
+          <v-btn dark icon v-bind="attrs" v-on="on">
             <v-icon>mdi-dots-vertical</v-icon>
           </v-btn>
         </template>
@@ -33,44 +25,46 @@
           </v-list-item>
         </v-list>
       </v-menu>
-
     </v-toolbar>
 
-    <v-dialog v-model="loginWin">
-      <v-row>
-        <v-col cols=10 ofset=1>
-          <v-card>
-            <v-card-title>
-              登录
-            </v-card-title>
-            <v-card-text>
+    <v-dialog v-model="loginWin" max-width="700">
+      <v-card>
+        <v-card-title> 登录 </v-card-title>
+
+        <v-card-text>
+          <v-row>
+            <v-col cols="10" ofset="1">
               <v-text-field label="用户名" v-model="userName"></v-text-field>
-              <v-text-field label="密码" v-model="passwd" type="password"></v-text-field>
-            </v-card-text>
-            <v-card-action>
-              <v-btn large>登录</v-btn>
-              <v-btn large @click="loginWin=false">取消</v-btn>
-            </v-card-action>
-          </v-card>
-        </v-col>
-      </v-row>
-      
+              <v-text-field
+                label="密码"
+                v-model="passwd"
+                type="password"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn large>登录</v-btn>
+          <v-btn large @click="loginWin = false">取消</v-btn>
+        </v-card-actions>
+      </v-card>
     </v-dialog>
   </v-card>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      loginWin:false,
-      userName:"",
-      passwd:"",
-    }
-  }
-}
+  data() {
+    return {
+      loginWin: false,
+      userName: "",
+      passwd: "",
+      backksrc:
+        "https://api.ri-co.cn/whisper/getpics/390d8fd0-VaranasiCandles_EN-AU12230572751_1920x1080.jpg",
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
