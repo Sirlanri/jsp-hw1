c09b9ba50c0e0d2095dd9c0923f852f83538b4ae
深蓝 <mail@ri-co.cn>
2020-11-05 11:43:46 +0800

    完成卡片

---
+++ src/components/itemCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card>
+    <v-img :src="picsrc" height="auto"></v-img>
+    <v-card-title>{{name}}</v-card-title>
+    <v-card-text>
+      {{detail}}
+      <br>
+      价格：{{price}}
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn large>
+        <v-icon>mdi-cart-outline</v-icon>加入
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+props:{
+  name:String,
+  detail:String,
+  picsrc:String,
+  price:Number,
+}
+}
+</script>
+
+<style scoped>
+.v-card{
+  margin: 1rem ;
+  padding: 1rem;
+}
+</style>
