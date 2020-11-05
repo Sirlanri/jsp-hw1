3d9d751560d739983f1169f8be3c00fb060407eb
深蓝 <mail@ri-co.cn>
2020-11-05 20:09:48 +0800

    添加商品

--- src/views/topbar.vue
+++ src/views/topbar.vue
@@ -23,7 +23,7 @@
         </template>
 
         <v-list>
-          <v-list-item link>
+          <v-list-item link @click="addItemWin=true">
             <v-list-item-title>添加</v-list-item-title>
           </v-list-item>
         </v-list>
@@ -47,11 +47,50 @@
           </v-row>
         </v-card-text>
         <v-card-actions>
-          <v-btn large>登录</v-btn>
-          <v-btn large @click="loginWin = false">取消</v-btn>
+          <v-btn large color="primary">登录</v-btn>
+          <v-btn large @click="loginWin = false" outlined>取消</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+    <v-dialog v-model="addItemWin" max-width="700">
+      
+
+      <v-card>
+        
+        <v-col cols="10" offset="1">
+        <v-card-title> 添加商品 </v-card-title>
+
+          <v-card-text>
+            <v-text-field
+              label="名称" v-model="addName"
+            ></v-text-field>
+            <v-textarea outlined
+              label="介绍" v-model="addDetail"
+            ></v-textarea>
+            <v-text-field
+              label="价格" type="number" v-model="addPrice"
+            ></v-text-field>
+            <v-text-field
+              label="图片链接" v-model="addPic" 
+            ></v-text-field>
+            <v-select
+              :items="tagsList"
+              v-model="addTag"
+              label="商品类型"
+            ></v-select>
+          </v-card-text>
+          
+          <v-card-actions>
+            <v-btn large @click="addItem" color="primary">添加</v-btn>
+            <v-btn large @click="addItemWin = false" outlined>取消</v-btn>
+          </v-card-actions>
+          </v-col>
+     
+        </v-card>
+      
+    </v-dialog>
+
   </v-card>
 
   <v-navigation-drawer v-model="leftDrawer" fixed style="top:64px">
@@ -61,7 +100,15 @@
           商品分类
         </v-list-item-title>
       </v-list-item>
-      <v-list-item link>
+      <v-list-item link @click="cTag('全部商品')">
+        <v-list-item-avatar>
+          <v-icon>mdi-all-inclusive</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          全部商品
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link @click="cTag('手机数码')">
         <v-list-item-avatar>
           <v-icon>mdi-cellphone-iphone</v-icon>
         </v-list-item-avatar>
@@ -69,7 +116,7 @@
           手机数码
         </v-list-item-content>
       </v-list-item>
-      <v-list-item link>
+      <v-list-item link @click="cTag('家用电器')">
         <v-list-item-avatar>
           <v-icon>mdi-hammer-wrench</v-icon>
         </v-list-item-avatar>
@@ -77,7 +124,7 @@
           家用电器
         </v-list-item-content>
       </v-list-item>
-      <v-list-item link>
+      <v-list-item link @click="cTag('汽车用品')">
         <v-list-item-avatar>
           <v-icon>mdi-car</v-icon>
         </v-list-item-avatar>
@@ -85,7 +132,7 @@
           汽车用品
         </v-list-item-content>
       </v-list-item>
-      <v-list-item link>
+      <v-list-item link  @click="cTag('服饰鞋帽')">
         <v-list-item-avatar>
           <v-icon>mdi-shoe-formal</v-icon>
         </v-list-item-avatar>
@@ -102,14 +149,46 @@
 export default {
   data() {
     return {
+      addItemWin:false,
       loginWin: false,
       leftDrawer:true,
       userName: "",
       passwd: "",
       backksrc:
         "https://api.ri-co.cn/whisper/getpics/390d8fd0-VaranasiCandles_EN-AU12230572751_1920x1080.jpg",
+      
+      tagsList:["手机数码","家用电器","汽车用品","服饰鞋帽"],
+      addName:"",
+      addPrice:0,
+      addPic:"",
+      addDetail:"",
+      addTag:"手机数码"
     };
   },
+
+  methods:{
+    addItem(){
+      let newItem={}
+      newItem.name=this.addName
+      newItem.price=this.addPrice
+      newItem.detail=this.addDetail
+      newItem.picsrc=this.addPic
+      newItem.tag=this.addTag
+      this.$store.commit('addItem',newItem)
+      //清空当前的数据
+      this.addName=""
+      this.addPrice=0
+      this.addPic=""
+      this.addDetail=""
+      this.addItemWin=false
+    },
+    cTag(tag){
+      this.$store.commit('cTag',tag)
+    }
+
+  },
+
+  
 };
 </script>
 
